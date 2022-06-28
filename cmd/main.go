cb6f3e26b2cca86ab0350c8c82da7319833b2347
Ruslan Kamenev <bigbobi004@gmail.com>
2022-06-29 00:12:00 +0300

    "Big changes"

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 	"webScraper/pkg/utils"
 )
 
@@ -13,64 +14,74 @@ type cntItemsFromOnePage struct {
 	NumFound int `json:"num_found"`
 }
 
-type Item struct {
-	Title string
-	Id    string
-}
-
 type page struct {
-	Items []Item `json:"documents"`
+	Items []struct {
+		Title string `json:"title"`
+		Id    string `json:"skucode"`
+	} `json:"documents"`
 }
 
 //Todo Возможно понадобиться изменить структуру для сбора цены (см. ответ запроса цены https://api.waters.com/waters-product-exp-api-v1/api/products/prices?customerNumber=anonymous&productNumber=186010094)
 
 type price struct {
-	value struct {
-		basePrice    float64
-		currencyCode string
-	}
+	Val struct {
+		Currency string  `json:"currencyCode"`
+		Value    float64 `json:"value"`
+	} `json:"basePrice"`
 }
 
-func (it *Item) UnmarshalJSON(data []byte) error {
-	var v []interface{}
-	if err := json.Unmarshal(data, &v); err != nil {
-		fmt.Printf("Error whilde decoding %v\n", err)
-		return err
+type availability struct {
+	Status string `json:"productStatus"`
+}
+
+type product struct {
+	Title        string
+	Id           string
+	Availability string
+	Price        struct {
+		Currency string
+		Value    float64
 	}
-	it.Title = v[2].(string)
-	it.Id = v[6].(string)
-	return nil
 }
 
 func main() {
-	fmt.Print(GetPriceOfOneItem("186010094"))
+	//fmt.Print(GetPriceOfOneItem("405000981"))
+	start := time.Now()
+	result, _ := GetAllDataFromOnePage(url)
+	duration := time.Since(start)
+	fmt.Println(duration)
+	fmt.Println(duration.Nanoseconds())
+	fmt.Print(result)
 }
 
 //Для того, чтобу узнать общее количество страниц товаров данного вида
-func GetPagesCnt(url string) (int, error) {
+func GetItemsCnt(url string) (int, error) {
 	body := utils.MakeRequest(url, nil, 0)
 	cnt := cntItemsFromOnePage{}
 	err := json.Unmarshal(body, &cnt)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return cnt.NumFound, nil
+}
 
+func GetPagesCnt(url string) (int, error) {
 	//Todo найти метод округления вверх
 	//Todo обработка ошибок
-	cntInt := int(cnt.NumFound/12 + 1)
+	cnt, _ := GetItemsCnt(url)
+	cntInt := int(cnt/12 + 1)
 	return cntInt, nil
 }
 
-/*
-func GetAllItemsFromOnePage(request string) ([]Item, error) {
-	body := MakeRequest(request)
+func GetAllItemsFromOnePage(request string) (page, error) {
+	body := utils.MakeRequest(request, nil, 0)
 	var pg page
 	//Todo доработать метод (https://stackoverflow.com/questions/42377989/unmarshal-json-array-of-arrays-in-go)
 	if err := json.Unmarshal(body, &pg); err != nil {
 		log.Fatal(err)
 	}
-	return pg.Items, nil
-}*/
+	return pg, nil
+}
 
 func GetPriceOfOneItem(itemId string) (price, error) {
 	const baseUrl = "https://api.waters.com/waters-product-exp-api-v1/api/products/prices?customerNumber=anonymous&productNumber="
@@ -81,9 +92,46 @@ func GetPriceOfOneItem(itemId string) (price, error) {
 		"language":    "en",
 	}
 	body := utils.MakeRequest(siteUrl, headers, 0)
-	var pr price
+	var pr []price
 	if err := json.Unmarshal(body, &pr); err != nil {
 		log.Fatal(err)
 	}
-	return pr, nil
+	return pr[0], nil
+}
+
+func GetAvailabilityOfOneItem(itemId string) (availability, error) {
+	//Todo узнать про страну запроса (от этого зависит результат)
+	siteUrl := fmt.Sprintf("https://prodservices.waters.com/api/waters/product/v1/availability/%s/US", itemId)
+	body := utils.MakeRequest(siteUrl, nil, 0)
+	var av availability
+	if err := json.Unmarshal(body, &av); err != nil {
+		log.Fatal(err)
+	}
+	return av, nil
+}
+
+func GetAllData(request string) ([]product, error) {
+	products := []product{}
+	page, _ := GetAllItemsFromOnePage(request)
+	for i := 0; i < len(page.Items); i++ {
+		itemId := page.Items[i].Id
+		//Todo распараллелить
+		price, _ := GetPriceOfOneItem(itemId)
+		availability, _ := GetAvailabilityOfOneItem(itemId)
+		products = append(products,
+			product{
+				Id:           page.Items[i].Id,
+				Title:        page.Items[i].Title,
+				Availability: availability.Status,
+				Price: struct {
+					Currency string
+					Value    float64
+				}(price.Val),
+			})
+	}
+	return products, nil
+}
+
+func GetAllData(request string) ([]product, error) {
+
 }
